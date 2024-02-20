3d8a2ee17aba96b32b14a27f28b6fabd013961f6
Chandraprakash Darji <prakashchandra3786@gmail.com>
2024-02-20 12:09:21 +0530

    fix: add contact card on every page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,8 +2,7 @@ import Image from 'next/image';
 
 import { CursorContainer } from '@/components/animation/cursor-container';
 import Heading from '@/components/heading';
-import { Icons } from '@/components/icons';
-import { UnderlineLink, UnstyledLink } from '@/components/links';
+import { UnderlineLink } from '@/components/links';
 
 import Faqs from '@/app/(marketing)/_components/faq';
 import Marquee from '@/app/(marketing)/_components/marquee';
@@ -11,7 +10,6 @@ import ProjectCard from '@/app/(marketing)/_components/project-card';
 import { Spotlight } from '@/app/(marketing)/_components/spotlight';
 import Time from '@/app/(marketing)/_components/time';
 import { projects } from '@/constant/projects';
-import { social } from '@/constant/social';
 
 const HomePage = () => {
   return (
@@ -174,40 +172,6 @@ const HomePage = () => {
             </Heading>
             <div className='mt-4 flex w-full flex-wrap gap-6 *:rounded-[45px] *:border'>
               <Faqs />
-            </div>
-          </div>
-        </section>
-        <section id='sec-contact' className='relative flex flex-col pt-32'>
-          <div className='layout relative z-10'>
-            <Heading>
-              Contact
-              {/* FEATURED /<ArrowLink href='/work'>WORK</ArrowLink> */}
-            </Heading>
-            <div className='mt-4 grid w-full grid-cols-1 gap-6 lg:grid-cols-5'>
-              <div className='bg-card rounded-lg p-6 lg:col-span-2'>
-                <h2 className='h1 font-normal'>Need a developer?</h2>
-                <p className='h4 text-muted-foreground mt-4 font-normal'>
-                  Shoot me a note with your project details at{' '}
-                  <UnstyledLink href='mailto:prakashchandra3786@gmail.com'>
-                    prakashchandra3786@gmail.com
-                  </UnstyledLink>
-                </p>
-              </div>
-              <div className='grid w-full grid-cols-2 gap-2 md:col-span-3'>
-                {social.map((item, index) => {
-                  const Icon = Icons[item.name];
-                  return (
-                    <UnderlineLink
-                      className='text-muted-foreground flex max-w-max gap-2 capitalize'
-                      href={item.url}
-                      key={index}
-                      trackEventTag={`Contact - ${item.name}`}
-                    >
-                      <Icon {...item} /> {item.name}
-                    </UnderlineLink>
-                  );
-                })}
-              </div>
             </div>
           </div>
         </section>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,29 +1,68 @@
 import React from 'react';
 
-import { UnderlineLink } from '@/components/links';
+import Heading from '@/components/heading';
+import { Icons } from '@/components/icons';
+import { UnderlineLink, UnstyledLink } from '@/components/links';
 
 import { links } from '@/constant/footer-links';
+import { social } from '@/constant/social';
 
 const Footer = () => {
   return (
-    <section className='relative flex flex-col pb-10 pt-20'>
-      <div className='border-t pt-3'>
-        <div className='mx-auto flex max-w-max flex-wrap items-center justify-center gap-5'>
-          {links.map((link) => (
-            <UnderlineLink
-              key={link.href}
-              href={link.href}
-              className='text-foreground hover:text-primary flex items-center py-1 font-mono text-sm capitalize'
-            >
-              {link.name}
-            </UnderlineLink>
-          ))}
+    <>
+      <section id='sec-contact' className='relative flex flex-col pt-32'>
+        <div className='layout relative z-10'>
+          <Heading>
+            Contact
+            {/* FEATURED /<ArrowLink href='/work'>WORK</ArrowLink> */}
+          </Heading>
+          <div className='mt-4 grid w-full grid-cols-1 gap-6 lg:grid-cols-5'>
+            <div className='bg-card rounded-lg p-6 lg:col-span-2'>
+              <h2 className='h1 font-normal'>Need a developer?</h2>
+              <p className='h4 text-muted-foreground mt-4 font-normal'>
+                Shoot me a note with your project details at{' '}
+                <UnstyledLink href='mailto:prakashchandra3786@gmail.com'>
+                  prakashchandra3786@gmail.com
+                </UnstyledLink>
+              </p>
+            </div>
+            <div className='grid w-full grid-cols-2 gap-2 md:col-span-3'>
+              {social.map((item, index) => {
+                const Icon = Icons[item.name];
+                return (
+                  <UnderlineLink
+                    className='text-muted-foreground flex max-w-max gap-2 capitalize'
+                    href={item.url}
+                    key={index}
+                    trackEventTag={`Contact - ${item.name}`}
+                  >
+                    <Icon {...item} /> {item.name}
+                  </UnderlineLink>
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </div>
-      <span className='mt-10 text-center font-mono tracking-wider'>
-        &copy; Chandra Prakash {new Date().getFullYear()}
-      </span>
-    </section>
+      </section>
+      <section className='relative flex flex-col pb-10 pt-20'>
+        <div className='border-t pt-3'>
+          <div className='mx-auto flex max-w-max flex-wrap items-center justify-center gap-5'>
+            {links.map((link) => (
+              <UnderlineLink
+                key={link.href}
+                href={link.href}
+                className='text-foreground hover:text-primary flex items-center py-1 font-mono text-sm capitalize'
+              >
+                {link.name}
+              </UnderlineLink>
+            ))}
+          </div>
+        </div>
+        <span className='mt-10 text-center font-mono tracking-wider'>
+          &copy; Chandra Prakash {new Date().getFullYear()}
+        </span>
+      </section>
+    </>
   );
 };
 
