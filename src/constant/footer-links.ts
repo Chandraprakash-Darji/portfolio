1cee5cfc5b5997721a4ebc5ab24c71420f5adefc
Chandraprakash Darji <prakashchandra3786@gmail.com>
2024-05-29 10:43:17 +0530

    chore: update source code link in footer

--- src/constant/footer-links.ts
+++ src/constant/footer-links.ts
@@ -1,5 +1,8 @@
 export const links = [
-  { name: 'Source Code', href: 'https://github.com/chandraprakash-darji' },
+  {
+    name: 'Source Code',
+    href: 'https://rega.lemonsqueezy.com/buy/8ffb3e31-3d6e-481d-9318-3b36f471e037',
+  },
   {
     name: 'Analytics',
     href: 'https://unami-kohl.vercel.app/share/Ltp2RrV5Q9i3Dn5H/Chandraprakash',
