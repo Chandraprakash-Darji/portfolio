fb90a2400cefae652e56199e79868924bc79c747
Chandraprakash Darji <prakashchandra3786@gmail.com>
2024-06-22 11:19:05 +0530

    better sorting with prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,27 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   arrowParens: 'always',
-  singleQuote: true,
-  jsxSingleQuote: true,
-  tabWidth: 2,
   semi: true,
-  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  tabWidth: 2,
+
+  importOrder: [
+    '^server-only|client-only$',
+    '^react$',
+    '^next(/.*)?$',
+    '<THIRD_PARTY_MODULES>',
+    '^@repo/(.*)$',
+    '^~/(.*)$',
+    '^[./]',
+  ],
+
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+
+  plugins: [
+    require('@trivago/prettier-plugin-sort-imports'),
+    'prettier-plugin-tailwindcss',
+  ],
+
+  tailwindAttributes: ['cn', 'cva'],
 };
