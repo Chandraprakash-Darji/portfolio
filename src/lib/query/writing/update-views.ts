3c954b1c1ca46f4a61d03d3d7f18ec21bec93e6d
Chandraprakash Darji <prakashchandra3786@gmail.com>
2024-06-24 17:11:24 +0530

    refactor: Update updateViews function to skip execution for admin users

--- src/lib/query/writing/update-views.ts
+++ src/lib/query/writing/update-views.ts
@@ -1,6 +1,13 @@
+import { currentRole } from '@/lib/auth/queries/current-role';
 import db from '@/lib/db';
+import { UserRole } from '@/lib/enums';
 
 const updateViews = async (postId: string) => {
+  const role = await currentRole();
+  if (role === UserRole.ADMIN) {
+    return;
+  }
+
   const updatedView = await db.post.update({
     where: { id: postId },
     data: {
