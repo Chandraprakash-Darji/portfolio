9d966bd41d7840b1245005117c367cd59b12371f
Chandraprakash Darji <prakashchandra3786@gmail.com>
2024-07-15 01:12:48 +0530

    feat: highlight

--- src/components/detail/writing/detail-post-content.tsx
+++ src/components/detail/writing/detail-post-content.tsx
@@ -6,6 +6,7 @@ import { defaultExtensions } from '@/components/protected/editor/wysiwyg/extensi
 import { isValidJson } from '@/lib/utils';
 import '@/styles/prosemirror.css';
 import { generateHTML } from '@tiptap/html';
+import hljs from 'highlight.js';
 
 const DetailPostContent = ({ content }: { content: string }) => {
   const json = useMemo(
@@ -17,9 +18,9 @@ const DetailPostContent = ({ content }: { content: string }) => {
     return generateHTML(json, defaultExtensions);
   }, [json]);
 
-  // React.useEffect(() => {
-  //   hljs.highlightAll();
-  // }, []);
+  React.useEffect(() => {
+    hljs.highlightAll();
+  }, []);
 
   return (
     <div
