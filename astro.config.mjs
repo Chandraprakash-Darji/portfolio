5edce15a12d9e4f7de627f991d810ccdcb0a7fc6
Chandraprakash-Darji <93640141+Chandraprakash-Darji@users.noreply.github.com>
2026-05-14 03:17:15 +0530

    fix: umami configration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { siteConfig } from './src/site.config';
+import umami from '@yeskunall/astro-umami';
 
 import vercel from '@astrojs/vercel';
 
@@ -31,6 +32,11 @@ export default defineConfig({
     sitemap(),
     tailwind({
       applyBaseStyles: false,
+    }),
+    umami({
+      id: '20fc61c2-6c23-40a3-9a46-5c215e1ca9f0',
+      endpointUrl: 'https://unami-kohl.vercel.app',
+      hostUrl: 'https://unami-kohl.vercel.app',
     }),
   ],
 
