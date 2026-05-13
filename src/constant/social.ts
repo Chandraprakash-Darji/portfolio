d35d637eb29bf73f4e92621ec859b5169df13e7a
Chandraprakash-Darji <93640141+Chandraprakash-Darji@users.noreply.github.com>
2026-05-13 13:42:33 +0530

    fix: removed the linkedin link from footer

--- src/constant/social.ts
+++ src/constant/social.ts
@@ -7,11 +7,7 @@ export const social = [
     name: 'twitter',
     url: 'https://twitter.com/chandra_7852',
   },
-  {
-    name: 'linkedIn',
-    url: 'https://www.linkedin.com/in/chandraprakash-darji/',
-  },
-  {
+{
     name: 'mail',
     url: 'mailto:prakashchandra3786@gmail.com',
   },
